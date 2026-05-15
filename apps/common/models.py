2980d39e2683dc7e3cac3164b23602d97b0b801c
asb <asbabushkin@yandex.ru>
2026-05-15 17:44:24 +0500

    Initial commit

---
+++ apps/common/models.py
@@ -0,0 +1,91 @@
+import uuid
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+from apps.common.managers import IsDeletedManager, GetOrNoneManager
+
+
+User = get_user_model()
+
+class BaseModel(models.Model):
+    """
+    A base model class that includes common fields and methods for all models.
+
+    Attributes:
+        id (UUIDField): Unique identifier for the model instance.
+        created_at (DateTimeField): Timestamp when the instance was created.
+        updated_at (DateTimeField): Timestamp when the instance was last updated.
+    """
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = GetOrNoneManager()
+
+    class Meta:
+        abstract = True
+
+
+
+class IsDeletedModel(BaseModel):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    objects = IsDeletedManager()
+
+    def delete(self, *args, **kwargs):
+        # Мягкое удаление is_deleted=True
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save(update_fields=["is_deleted", "deleted_at"])
+
+    def hard_delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+
+
+class Content(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, blank=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="Контент", verbose_name="Автор")
+    metadata = models.JSONField(default=dict, blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+
+class Article(Content):
+    text = models.TextField()
+    word_count = models.IntegerField(blank=True)
+
+    def save(self, *args, **kwargs):
+        text = self.text
+        self.word_count = len(text.split())
+        super().save(*args, **kwargs)
+
+
+
+class Video(Content):
+    video_url = models.URLField()
+    duration = models.DurationField(null=True, blank=True)
+
+class Image(Content):
+    image = models.ImageField(upload_to="images/")
+    dimensions = models.CharField(max_length=255, blank=True, null=True)
+
+
+
+
---
+++ apps/profiles/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+from apps.accounts.models import User
+from apps.common.models import BaseModel
+
+
+class ShippingAddress(BaseModel):
+    """
+    Represents a shipping address associated with a user.
+
+    Attributes:
+        user (ForeignKey): The user who owns the shipping address.
+        full_name (str): The full name of the recipient.
+        email (str): The email address of the recipient.
+        phone (str): The phone number of the recipient.
+        address (str): The street address of the recipient.
+        city (str): The city of the recipient.
+        country (str): The country of the recipient.
+        zipcode (str): The postal code of the recipient.
+
+    Methods:
+        __str__():
+            Returns a string representation of the shipping details.
+    """
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="shipping_addresses"
+    )
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"{self.full_name}'s shipping details"
